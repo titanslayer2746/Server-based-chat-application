1d9e31ff8909464251093c4a3a8bc549a510045f
Yatin Kumar <yatin.2022ug1074@iiitranchi.ac.in>
2024-12-14 09:56:21 +0530

    Initial Project Setup

---
+++ app/(main)/page.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@/components/ui/button";
+
+
+export default function Home() {
+  return (
+    <div>
+      <p className="text-3xl font-bold text-indigo-500">
+        Chat app
+        </p>
+      <Button>
+        Click Me
+      </Button>
+    </div>
+    
+  );
+}
